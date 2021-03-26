4632301af60e6d05a21083c953a5f92f9ac7267f
Hacnine <hacninetusar@gmail.com>
2021-03-26 08:30:51 +0600

    Django Rough Project

--- Rest CRUD/accounts/admin.py
+++ Rest CRUD/accounts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
+from accounts.models import *
 
-# admin.site.register(Course)
+admin.site.register(Student)
 # admin.site.register(CourseModule)
 # admin.site.register(Profile)
 
--- Rest CRUD/accounts/views.py
+++ Rest CRUD/accounts/views.py
@@ -1,22 +1,30 @@
-from datetime import datetime, timedelta
-import stripe
+import io
+
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from rest_framework import serializers
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
+
+from accounts.models import *
+from accounts.serializers import StudentSerializer
 
 
-def store(request):
-    context = {}
-    return render(request, 'store.html', context)
+def student(request):
 
+    if request.method == 'GET':
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        python_data = JSONParser().parse(stream)
+        id = python_data.get('id', None)
+        if id is not None:
+            stu = Student.objects.get(id=id)
+            serializr = StudentSerializer(stu)
+            json_data = JSONRenderer().render(serializr.data)
+            return HttpResponse(json_data, content_type='application/json')
 
-def cart(request):
-
-    context = {}
-    return render(request, 'cart.html', context)
-
-
-def checkout(request):
-
-    context = {}
-    return render(request, 'checkout.html', context)
-
+    stu = Student.objects.all()
+    serializr = StudentSerializer(stu, many=True)
+    json_data = JSONRenderer().render(serializr.data)
+    return HttpResponse(json_data, content_type='application/json')
 
