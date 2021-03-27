0bc365c12ea3878ab947dba9905e2a08b5be38ec
Hacnine <hacninetusar@gmail.com>
2021-03-27 07:52:43 +0600

    Django Rough Project

--- Rest CRUD/accounts/views.py
+++ Rest CRUD/accounts/views.py
@@ -1,7 +1,8 @@
 import io
 
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework import serializers
 from rest_framework.parsers import JSONParser
 from rest_framework.renderers import JSONRenderer
@@ -10,6 +11,7 @@ from accounts.models import *
 from accounts.serializers import StudentSerializer
 
 
+@csrf_exempt
 def student(request):
 
     if request.method == 'GET':
@@ -23,8 +25,57 @@ def student(request):
             json_data = JSONRenderer().render(serializr.data)
             return HttpResponse(json_data, content_type='application/json')
 
-    stu = Student.objects.all()
-    serializr = StudentSerializer(stu, many=True)
-    json_data = JSONRenderer().render(serializr.data)
-    return HttpResponse(json_data, content_type='application/json')
+        else:
+            stu = Student.objects.all()
+            serializr = StudentSerializer(stu, many=True)
+            json_data = JSONRenderer().render(serializr.data)
+            return HttpResponse(json_data, content_type='application/json')
+
+    if request.method == 'POST':
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        python_data = JSONParser().parse(stream)
+        serializr = StudentSerializer(data=python_data)
+
+        if serializr.is_valid():
+            serializr.save()
+            response = {'msg': 'Data inserted.'}
+            json_data = JSONRenderer().render(response)
+            return HttpResponse(json_data, content_type='application/json')
+        else:
+            json_data = JSONRenderer().render(serializr.errors)
+            return HttpResponse(json_data, content_type='application/json')
+
+    if request.method == 'PUT':
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        python_data = JSONParser().parse(stream)
+        id = python_data.get('id')
+        stu = Student.objects.get(id=id)
+        serializr = StudentSerializer(stu, data=python_data,
+                                      partial=True)
+
+        if serializr.is_valid():
+            serializr.save()
+            response = {'msg': 'Data updated.'}
+            json_data = JSONRenderer().render(response)
+            return HttpResponse(json_data, content_type='application/json')
+        # else:
+        json_data = JSONRenderer().render(serializr.errors)
+        return HttpResponse(json_data, content_type='application/json')
+
+    if request.method == 'DELETE':
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        python_data = JSONParser().parse(stream)
+        id = python_data.get('id')
+        stu = Student.objects.get(id=id)
+        stu.delete()
+
+        response = {'msg': 'Data deleted.'}
+        # json_data = JSONRenderer().render(response)
+        # return HttpResponse(json_data, content_type='application/json')
+
+        return JsonResponse(response, safe=False)
+
 
--- Rest CRUD/myapp.py
+++ Rest CRUD/myapp.py
@@ -16,4 +16,48 @@ def get_data(id=None):
     print(data)
 
 
-get_data(1)
+# get_data(2)
+
+def post_data():
+    data = {
+        'name': 'Jabir',
+        'roll': 104,
+        'city': 'Khulna'
+    }
+    json_data = json.dumps(data)
+    request = requests.post(url=URL, data=json_data)
+    data = request.json()
+    print(data)
+
+
+# post_data()
+
+
+def update_data():
+    data = {
+        'id': 2,
+        'name': 'Abir',
+        'city': 'Jessore'
+    }
+    json_data = json.dumps(data)
+    req = requests.put(url=URL, data=json_data)
+    data = req.json()
+    print(data)
+
+
+# update_data()
+
+
+def delete_data():
+    data = {
+        'id': 3,
+    }
+    json_data = json.dumps(data)
+    req = requests.delete(url=URL, data=json_data)
+    data = req.json()
+    print(data)
+
+
+delete_data()
+
+
