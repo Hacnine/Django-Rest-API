9cc2d771d4a733e1f2444d8b95781a6d34da28fb
Hacnine <hacninetusar@gmail.com>
2021-04-11 14:44:58 +0600

    'Django Rough Project'

---
+++ Filtering and django filter/accounts/models.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    roll = models.IntegerField()
+    city = models.CharField(max_length=100)
+    passby = models.CharField(max_length=50, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+
---
+++ Filtering and django filter/accounts/views.py
@@ -0,0 +1,35 @@
+from django.views.generic import *
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets
+from rest_framework.generics import ListAPIView
+from rest_framework.permissions import *
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.throttling import *
+
+
+# # One way to filter
+# class StudentList(ListAPIView):
+#     queryset = Student.objects.all()
+#     serializer_class = StudentSerializer
+#
+#     def get_queryset(self):
+#         user = self.request.user
+#         return Student.objects.filter(passby=user)
+
+
+# Another way to filter
+
+class StudentList(ListAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    # if you want to filter locally
+    filter_backends = [DjangoFilterBackend]
+    filter_fields = ['name', 'city']
+
+    # how to filter
+    #  http://127.0.0.1:8000/api/?name=Tanvir&city=Sylhe
+
+
